50b88afe01efc10c7d69eb56a2cfa7bd3d0f5762
Saskia <skuhlmann@1drop.de>
2021-11-05 15:42:18 +0100

    finished quicksort chapter
    added summary of the quicksort chapter and the quicksort algorythm

--- src/php/algorythms/quicksort/index.php
+++ src/php/algorythms/quicksort/index.php
@@ -1,4 +1,66 @@
 <?php
+// Divide and Conquer -> divide problems in smaller problems with a base case as the simplest case
+// and recursive case that makes the left over probem smaller and smaller until it reaches the base case (Euklid)
+// with arrays the base case is often an empty array or with just 1 element
+
+//Quicksort
+// involves a pivot = picked element to compare other elements with to partition the array into sub-arrays, which
+// eventually leads to the array being sorted
+// the speed depends on the pivot
+// merge sort algorithm (O(n log n)) is in worst case faster than quick sort (O(n²)) but in average quicksort is faster
+// (O (n log n))
+// sometimes a constant c multiplied with n makes a difference in speed of 2 different algorithms, but its usually ignored
+// quick sort has a smaller constant than merge sort -> it is faster in average cases (depending on where the pivot is
+// located in the array -> faster when it is in the middle -> choose random element in average case
+
+function quickSort($arrQuick)
+{
+    $smallerArr = [];
+    $biggerArr = [];
+    if (count($arrQuick) < 2) {
+        return $arrQuick;
+    }
+    $pivot = $arrQuick[0];
+
+    foreach ($arrQuick as $key => $num) {
+        if ($key === 0) {
+            continue;
+        }
+
+        if ($num <= $pivot) {
+            $smallerArr[] = $num;
+            continue;
+        }
+        $biggerArr[] = $num;
+    }
+
+
+    $arrPiv[] = $pivot;
+
+    if (count($smallerArr) === 0 && count($biggerArr) > 0) {
+
+        array_unshift($biggerArr, $pivot);
+        echo "<pre>";
+        //var_dump($biggerArr);
+        echo "</pre>";
+        return $biggerArr;
+
+    } elseif (count($smallerArr) > 0 && count($biggerArr) === 0) {
+        $smallerArr[] = $pivot;
+        echo "<pre>";
+        //var_dump($smallerArr);
+        echo "</pre>";
+        return $smallerArr;
+    }
+
+    $mergedArr = array_merge(quickSort($smallerArr), $arrPiv, quickSort($biggerArr));
+    //var_dump($mergedArr);
+    return $mergedArr;
+
+
+}
+
+var_dump(quickSort([3, 1]));
 
 //Exercises from that chapter:
 
@@ -57,7 +119,8 @@ function maxNum($numbers)
 //var_dump(maxNum([2, 4, 1, 3]));
 
 //4. base case and recursive case for binary search
-function binarySearch($array, $x) {
+function binarySearch($array, $x)
+{
 
     //base case
     if (count($array) === 1) {
@@ -69,22 +132,22 @@ function binarySearch($array, $x) {
 
     //recursive case
 // funktioniert nur mit true oder false, sonst zu kompliziert
-    /*
+
     $low = 0;
     $high = count($array) - 1;
-    $mid = floor(($low + $high)/2);
+    $mid = floor(($low + $high) / 2);
     print($mid . "<br>");
     if ($array[$mid] === $x) {
         return $array[$mid];
     } elseif ($array[$mid] < $x) {
-        $arrayHalf = array_slice($array, $mid+1, $mid+1);
+        $arrayHalf = array_slice($array, $mid + 1, $mid + 1);
         echo "arraz < x: ";
-       var_dump($arrayHalf);
-       echo "<br>";
+        var_dump($arrayHalf);
+        echo "<br>";
         return binarySearch($arrayHalf, $x);
     }
 
-    $arrayHalf = array_slice($array, 0, $mid+1);
+    $arrayHalf = array_slice($array, 0, $mid + 1);
     echo "arraz < x";
     var_dump($arrayHalf);
     echo "<br>";
@@ -92,5 +155,4 @@ function binarySearch($array, $x) {
 
 }
 
-var_dump(binarySearch([1, 2, 3], 1));
-    */
+//var_dump(binarySearch([1, 2, 3], 1));
